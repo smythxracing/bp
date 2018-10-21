29f3bd32b07918bdb342764192ca22f4ea78634f
smythxracing <j.smyth.it@gmail.com>
2018-10-21 12:06:14 +0100

    adding more code coverage

--- BPCalculator.Tests/BloodPressure_BPCategory.test.cs
+++ BPCalculator.Tests/BloodPressure_BPCategory.test.cs
@@ -15,7 +15,18 @@ namespace Tests
         }
 
         [Test]
-        public void BP_is_Low()
+        public void Systolic_Low_BP_is_Low()
+        {
+            _bp.Systolic= 80;
+            _bp.Diastolic= 80;
+            var actual = _bp.Category;
+            var expected = BPCategory.Low;
+            
+            Assert.AreEqual(expected, actual );
+        }
+
+        [Test]
+        public void Diastolic_Low_BP_is_Low()
         {
             _bp.Systolic= 120;
             _bp.Diastolic= 50;
@@ -36,10 +47,10 @@ namespace Tests
             Assert.AreEqual(expected, actual );
         }
         [Test]
-        public void BP_is_Pre_High()
+        public void Systolic_Pre_High_BP_is_Pre_High()
         {
             _bp.Systolic= 130;
-            _bp.Diastolic= 70;
+            _bp.Diastolic= 80;
             var actual = _bp.Category;
             var expected = BPCategory.PreHigh;
             
@@ -47,9 +58,31 @@ namespace Tests
         }
 
         [Test]
-        public void BP_is_High()
+        public void Diastolic_Pre_High_BP_is_Pre_High()
+        {
+            _bp.Systolic= 110;
+            _bp.Diastolic= 85;
+            var actual = _bp.Category;
+            var expected = BPCategory.PreHigh;
+            
+            Assert.AreEqual(expected, actual );
+        }
+
+        [Test]
+        public void Systolic_High_BP_is_High()
         {
             _bp.Systolic= 170;
+            _bp.Diastolic= 75;
+            var actual = _bp.Category;
+            var expected = BPCategory.High;
+            
+            Assert.AreEqual(expected, actual );
+        }
+
+        [Test]
+        public void BP_is_High()
+        {
+            _bp.Systolic= 120;
             _bp.Diastolic= 92;
             var actual = _bp.Category;
             var expected = BPCategory.High;
