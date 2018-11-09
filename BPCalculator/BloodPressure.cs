5f6b144cf859ab5508533e867704260b2887d90a
smythxracing <j.smyth.it@gmail.com>
2018-11-09 18:15:29 +0000

    Adding new suggested treatment feature

--- BPCalculator/BloodPressure.cs
+++ BPCalculator/BloodPressure.cs
@@ -12,12 +12,22 @@ namespace BPCalculator
         [Display(Name = "High Blood Pressure")] High
     };
 
+    public enum BPSuggestedTreatment
+    {
+        [Display(Name = "Eat More Salt")] Low,
+        [Display(Name = "Keep up the healthy lifestyle")] Normal,
+        [Display(Name = "Time to ease back on the booze and cigarettes")] PreHigh,
+        [Display(Name = "Time to visit the GP for a checkup")] High
+    };
+
     public class BloodPressure
     {
         public const int SystolicMin = 70;
         public const int SystolicMax = 190;
         public const int DiastolicMin = 40;
         public const int DiastolicMax = 100;
+
+        public String CurrentBP;
 
         [Range(SystolicMin, SystolicMax, ErrorMessage = "Invalid Systolic Value")]
         public int Systolic { get; set; }                       // mmHG
@@ -46,6 +56,34 @@ namespace BPCalculator
                 else if ((Systolic > 120 && Systolic <= 140) || (Diastolic > 80 && Diastolic <= 90))
                 {
                     return BPCategory.PreHigh;
+                }
+                else
+                {
+                    throw new Exception("Out of BP Category range");
+                }
+            }
+        }
+
+        public BPSuggestedTreatment SuggestedTreatment
+        {
+            get
+            {
+                // implement as part of project
+                if (Systolic < 90 || Diastolic < 60)
+                {
+                    return BPSuggestedTreatment.Low;
+                }
+                else if ((Systolic > 140 && Systolic <= 190) || Diastolic > 90)
+                {
+                    return BPSuggestedTreatment.High;
+                }
+                else if ((Systolic >= 90 && Systolic <= 120) && (Diastolic >= 60 && Diastolic <= 80))
+                {
+                    return BPSuggestedTreatment.Normal;
+                }
+                else if ((Systolic > 120 && Systolic <= 140) || (Diastolic > 80 && Diastolic <= 90))
+                {
+                    return BPSuggestedTreatment.PreHigh;
                 }
                 else
                 {
